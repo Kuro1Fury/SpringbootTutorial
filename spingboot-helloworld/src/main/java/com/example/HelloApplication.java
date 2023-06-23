c7dbaf80017f1a1baedc1e8b6dc0663596657d7d
Kuro1Fury <skycoder0820@gmail.com>
2023-06-23 18:11:35 +0800

    finish simple demo

---
+++ spingboot-helloworld/src/main/java/com/example/HelloApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Boot Class. Starting point of the application.
+ */
+@SpringBootApplication
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
