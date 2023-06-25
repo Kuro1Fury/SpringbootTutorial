d90fae151ef2948cc595fa9bdfacd2cc9294c744
Kuro1Fury <skycoder0820@gmail.com>
2023-06-25 15:10:05 +0800

    finish condition

--- springboot-condition/src/main/java/com/example/springbootcondition/condition/ClassCondition.java
+++ springboot-condition/src/main/java/com/example/springbootcondition/condition/ClassCondition.java
@@ -4,18 +4,45 @@ import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
+import java.util.Map;
+
 public class ClassCondition implements Condition {
+    /**
+     * @param context Springboot context. Get bean factory, environment, bean definition registry
+     * @param metadata Annotation metadata. Get annotation attributes. Get class name from annotation attributes
+     * @return
+     */
+//    @Override
+//    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+//        // Demand 1: Create bean after get Jedis
+//        // Check redis.clients.jedis.Jedis.class exists or not
+//        boolean flag = true;
+//        try {
+//            Class<?> clazz = Class.forName("redis.clients.jedis.Jedis");
+//            System.out.println("jedis class exists");
+//        } catch (ClassNotFoundException e) {
+//            flag = false;
+//            System.out.println("jedis class not exists");
+//        }
+//        return flag;
+//    }
+
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        // 1. Demand: Create bean after get Jedis
-        // Check redis.clients.jedis.Jedis.class exists or not
+        // Demand 2: Create bean after get annotation value
+
+        // Get annotation attributes
+        Map<String, Object> map = metadata.getAnnotationAttributes(ConditionOnClass.class.getName());
+//        System.out.println(map);
+        String[] value = (String[]) map.get("value");
         boolean flag = true;
         try {
-            Class<?> clazz = Class.forName("redis.clients.jedis.Jedis");
-            System.out.println("jedis class exists");
+            for (String className : value) {
+                Class<?> clazz = Class.forName(className);
+                System.out.println(className + " exists");
+            }
         } catch (ClassNotFoundException e) {
             flag = false;
-            System.out.println("jedis class not exists");
         }
         return flag;
     }
--- springboot-condition/src/main/java/com/example/springbootcondition/config/UserConfig.java
+++ springboot-condition/src/main/java/com/example/springbootcondition/config/UserConfig.java
@@ -1,7 +1,9 @@
 package com.example.springbootcondition.config;
 
 import com.example.springbootcondition.condition.ClassCondition;
+import com.example.springbootcondition.condition.ConditionOnClass;
 import com.example.springbootcondition.domain.User;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +12,15 @@ import org.springframework.context.annotation.Configuration;
 public class UserConfig {
 
     @Bean
-    @Conditional(ClassCondition.class)
+    // @Conditional(ClassCondition.class)
+//    @ConditionOnClass("redis.clients.jedis.Jedis")
     public User user() {
+        return new User();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "kuro", havingValue = "kurofury")
+    public User user2() {
         return new User();
     }
 }
